243ae0abc297d051b55cb7979e218b3a451e6f49
hunk <hunk@heimavista.cn>
2018-11-22 14:14:42 +0800

    test

---
+++ git2.sh
@@ -0,0 +1,27 @@
+
+yum remove -y git
+
+yum install -y  http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-1.noarch.rpm
+
+yum install git -y
+
+git config --global user.email "hunk@heimavista.cn"
+git config --global user.name "hunk"
+
+#git 初始化仓库
+#
+git init
+
+git add
+
+git commit
+
+git status
+
+git log
+
+git reflog
+
+git reset --hard commit_id
+
+#
